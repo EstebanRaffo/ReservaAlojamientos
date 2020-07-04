a99075432b93b2011aa4490ea612dbc396429544
unknown <e.fraffo@gmail.com>
2020-07-04 20:26:12 -0300

    4/07

--- scripts/components/DateFilter.jsx
+++ scripts/components/DateFilter.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class DateFilter extends Component{
     constructor(props){
@@ -13,12 +12,7 @@ class DateFilter extends Component{
                 <div className="control has-icons-left">
                     <input className="input" type="date" onChange={onDateChange} value={date} name={name}/> 
                     <span className="icon is-small is-left">
-                        {/* <FontAwesomeIcon icon={`fas ${this.props.icon}`} /> */}
                         <i className={`fas ${icon}`}></i>
-                       
-                        {/* Para mostrar el ícono la solución es simple: con que reciba como parámetro la cadena de texto fa-sign-in-alt para el 
-                        caso de la fecha de ingreso y fa-sign-out-alt para la fecha de egreso, ya podríamos concatenar al elemento <i className="fas"></i>
-                         dicho valor en el atributo className. */}
                     </span>
                 </div>
             </div>
--- scripts/components/OptionsFilter.jsx
+++ scripts/components/OptionsFilter.jsx
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 
 class OptionsFilter extends Component{
     constructor(props){
